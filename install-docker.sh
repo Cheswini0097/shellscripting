3a09f7996d1f0c96277a41eb75182d17a3510873
chethan <cheswinreddy@gmail.com>
2024-11-13 19:49:05 +0530

    jenkins

--- install-docker.sh
+++ install-docker.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-sudo yum update -y
-sudo amazon-linux-extras install docker
-sudo yum install -y docker
-sudo service docker start
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
